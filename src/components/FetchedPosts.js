984939547f7010995dd5f79f6297b13789da2985
AnatoliyNBulyga <anatoliynbulyga@gmail.com>
2020-05-31 21:15:55 +0300

    init

---
+++ src/components/FetchedPosts.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Post from "./Post";
+
+const FeatchedPosts =  ({posts}) => {
+    if (!posts.length) {
+        return <button className="btn btn-primary">Загрузить</button>
+    }
+    return posts.map( post => <Post post={post} key={post} />)
+};
+export default FeatchedPosts;
